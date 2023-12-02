397cf73d08adfce275cd186c1f78cdef37450a06
so_hell <armanyppph@gmail.com>
2023-12-02 21:23:01 +0530

    final commit

--- src/ui/home/faq.tsx
+++ src/ui/home/faq.tsx
@@ -6,36 +6,36 @@ import { VStack } from "@chakra-ui/react";
 import Container from "@/src/ui/primitives/Container";
 import Accordion from "@/src/ui/primitives/Accordian";
 
-const FAQ = () => {
-  const faqData: { title: string; content: string }[] = [
-    {
-      title: "What is Manage Wise and what does it offer?",
-      content:
-        "Manage Wise is a comprehensive management platform designed to streamline your business operations, enhance productivity, and drive success. It offers a range of features including task management, scheduling, communication tools, analytics, and more.",
-    },
-    {
-      title:
-        "Is Manage Wise suitable for small businesses and larger enterprises alike?",
-      content:
-        "Yes, Manage Wise is designed to cater to the needs of both small businesses and larger enterprises. Its scalable features and customizable options make it versatile for various business sizes.",
-    },
-    {
-      title: "Can I access Manage Wise from different devices and platforms?",
-      content:
-        "Yes, Manage Wise offers cross-platform compatibility, allowing you to access and use the platform seamlessly on desktops, web browsers, and mobile devices.",
-    },
-    {
-      title: "What kind of support options do you offer to users?",
-      content:
-        "We provide responsive customer support via chat and email to assist you with any inquiries, issues, or guidance you may need while using Manage Wise.",
-    },
-    {
-      title: "How secure is the data stored within Manage Wise?",
-      content:
-        "Your data's security is a top priority. Manage Wise employs robust data encryption and follows industry best practices to ensure your sensitive information is safe and protected.",
-    },
-  ];
+const faqData: { title: string; content: string }[] = [
+  {
+    title: "What is Manage Wise and what does it offer?",
+    content:
+      "Manage Wise is a comprehensive management platform designed to streamline your business operations, enhance productivity, and drive success. It offers a range of features including task management, scheduling, communication tools, analytics, and more.",
+  },
+  {
+    title:
+      "Is Manage Wise suitable for small businesses and larger enterprises alike?",
+    content:
+      "Yes, Manage Wise is designed to cater to the needs of both small businesses and larger enterprises. Its scalable features and customizable options make it versatile for various business sizes.",
+  },
+  {
+    title: "Can I access Manage Wise from different devices and platforms?",
+    content:
+      "Yes, Manage Wise offers cross-platform compatibility, allowing you to access and use the platform seamlessly on desktops, web browsers, and mobile devices.",
+  },
+  {
+    title: "What kind of support options do you offer to users?",
+    content:
+      "We provide responsive customer support via chat and email to assist you with any inquiries, issues, or guidance you may need while using Manage Wise.",
+  },
+  {
+    title: "How secure is the data stored within Manage Wise?",
+    content:
+      "Your data's security is a top priority. Manage Wise employs robust data encryption and follows industry best practices to ensure your sensitive information is safe and protected.",
+  },
+];
 
+const FAQ = () => {
   return (
     <Container>
       <Flex
